c3d4359a21624e29cb950140e378f2a6c2c1c1e0
Aarish Rahman <rahman-aarish19@github.com>
2020-10-24 21:19:24 +0530

    removed app secret

--- config.js
+++
@@ -1,7 +0,0 @@
-const str = {
-  //"connectionString": "mongodb+srv://interview:welcome2byteridge@byteridge-hdrl6.mongodb.net/interview",
-  "connectionString": "mongodb://localhost:27017/byteridgetest",
-  "secret": "JBJYBJ23jkj232kALKMLAK"
-}
-
-module.exports = str;
